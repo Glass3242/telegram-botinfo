f1f8c83acec32a1ce1f0c940105f0ce8496e72c1
Glass3242 <160028529+Glass3242@users.noreply.github.com>
2024-03-19 17:20:17 +0300

    Update свмвва.py

--- свмвва.py
+++ свмвва.py
@@ -54,9 +54,13 @@ async def cmd_smart(message: types.Message):
     await message.answer('\n1. Shieldwall \n2. Thief Simulator 2 \n3. Bum Simulator \n4. Farmer Life \n5. Contraband Police \n6. EA Sports FC 24 \n7. Car for Sale Simulator 2023 \n8. House Flipper 2 \n9. Russian Train Trip 2 \n10. NBA 2K24 \n11. Metro Simulator 2 \n12. Euro Truck Simulator 2 \n13. In Truck Driving \14. Arma Reforger ')
 
 @dp.message(Command('racinggames'))
-async def cmd_smart(message: types.Message)
-    await message.answer('')
+async def cmd_smart(message: types.Message):
+    await message.answer('\n1. Forza Horizon 5 \n2. Need for speed Heat \n3. Gran turismo 7 \n4. The crew 2 \n5 Assetto Corsa Competizione \n6. Forza Motorsport 7 \n7. Grid 2 \n8. The Crew \n9. Dirt Rally 2 \n10. Shift 2 Unleashed')
 # Запуск процесса поллинга новых апдейтов
+
+@dp.message(Command('fantasygames'))
+async def cmd_smart(message:types.Message):
+    await message.answer('\n1. Genshin Impact \n2. Dota 2 \n3. World of Warcraft \n4. Bloodborne \n5. The Wither 2 \n6. The wither 3 \n7 Dark Souls 2 \n8. Hollow Knight \n9. Sekiro: Shadows Die Twice \n10. Nioh')
 async def main():
     await dp.start_polling(bot)
 
